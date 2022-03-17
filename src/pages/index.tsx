ba1eee535e5d9804a0bbaafab999d1806bd1dcc6
Lucas Trecente <lucastrecente@hotmail.com>
2022-03-17 03:00:47 -0300

    feat: stripe api integration

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,21 @@
+import { GetStaticProps } from "next";
 import Head from "next/head";
 import Image from "next/image";
+
 import { SubscribeButton } from "../components/SubscribeButton";
+import { stripe } from "../services/stripe";
+
 import avatarImg from "../../public/images/avatar.png";
 import styles from "./home.module.scss";
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  };
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
@@ -19,12 +30,31 @@ export default function Home() {
           </h1>
           <p>
             Get access to all the publications <br />
-            <span>for $4.99 month</span>
+            <span>for {product.amount} month</span>
           </p>
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceId} />
         </section>
         <Image src={avatarImg} alt="Akame ga kill" quality={100} />
       </main>
     </>
   );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve("price_1Ke6jWDhJDsnmuxOSnZQtV00");
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+    }).format(price.unit_amount / 100),
+  };
+
+  return {
+    props: {
+      product,
+    },
+    revalidate: 60 * 60 * 24, // 24 hours
+  };
+};
