1b8b0be77ea18b01d6308979bfbb06812bfcce4d
Lucas Trecente <lucastrecente@hotmail.com>
2022-03-17 07:11:30 -0300

    fix: version error

--- src/services/stripe.ts
+++ src/services/stripe.ts
@@ -1,10 +1,10 @@
 import Stripe from "stripe";
-import { version  } from "../../package.json";
+import packageInfo from "../../package.json";
 
 export const stripe = new Stripe(process.env.STRIPE_API_KEY, {
   apiVersion: "2020-08-27",
   appInfo: {
-    name: "anime.news",
-    version
+    name: packageInfo.name,
+    version: packageInfo.version,
   },
 });
