ab4868ab4829f625ba948f95614e8254bca29703
Lucas Trecente <lucastrecente@hotmail.com>
2022-03-18 07:59:05 -0300

    feat: add payment in stripe

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,3 +1,7 @@
+import { useSession, signIn } from "next-auth/react";
+import { api } from "../../services/api";
+import { getStripeJs } from "../../services/stripe-js";
+import toast from "react-hot-toast";
 import styles from "./style.module.scss";
 
 interface SubscribeButtonProps {
@@ -5,8 +9,35 @@ interface SubscribeButtonProps {
 }
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+  const { data: session } = useSession();
+
+  async function handleSubscribe() {
+    if (!session) {
+      signIn("github");
+      return;
+    }
+
+    try {
+      const response = await api.post("/checkoutSubscribe");
+
+      const { sessionId } = response.data;
+
+      toast.success("Redirecionando para checkout!");
+
+      const stripe = await getStripeJs();
+
+      await stripe.redirectToCheckout({ sessionId });
+    } catch (err) {
+      toast.error("Ocorreu um erro.");
+    }
+  }
+
   return (
-    <button type="button" className={styles.subscribeBtn}>
+    <button
+      type="button"
+      className={styles.subscribeBtn}
+      onClick={handleSubscribe}
+    >
       Subscribe now
     </button>
   );
