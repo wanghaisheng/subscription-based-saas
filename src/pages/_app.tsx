01ef3fe8f31cb55ff4c52465dde99f21d73b06ea
Lucas Trecente <lucastrecente@hotmail.com>
2022-03-18 07:58:17 -0300

    feat: add react-hot-toast

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,14 @@
 import { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
 import { Header } from "../components/Header";
+import { Toaster } from "react-hot-toast";
 
 import "../styles/global.scss";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <SessionProvider session={pageProps.session}>
+      <Toaster />
       <Header />
       <Component {...pageProps} />
     </SessionProvider>
